34b7c139ed5a8bbbc954f71cd4c16d80c3886f63
dodody <dody@bigpearl.io>
2020-08-27 23:37:06 +0900

    코드 추가

--- jsCodeFestival/code11_20.js
+++ jsCodeFestival/code11_20.js
@@ -27,14 +27,39 @@
   // console.log(planets[q-1])
 
 // 14 
-  let d14 = prompt('숫자는 3의 배수인가요?');
-
-  if(d14%3 ===0) {
-    console.log('짝')
-  }else{
-    console.log(d14)
-  }
+  // let d14 = prompt('숫자는 3의 배수인가요?');
+  // if(d14%3 ===0) {
+  //   console.log('짝')
+  // }else{
+  //   console.log(d14)
+  // }
 
 // 15
-  let name = prompt('자기소개 하기')
-  console.log('안녕하세요 저는' + name + '입니다')
+  // let name = prompt('자기소개 하기')
+  // console.log('안녕하세요 저는' + name + '입니다')
+
+// 16
+  // const d16 = prompt('입력하세요')
+  // const dd16 = d16.split('').reverse().join('');
+  // console.log(dd16)
+
+// 17 
+  // let d17 = prompt('당신의 키는?');
+  // if(d17 >= 150){
+  //   console.log('yes')
+  // }else{
+  //   console.log('no')
+  // }
+
+// 18 
+  let d18 = prompt('입력하세요 평균을 내 드릴게요!');
+  let dd18 = d18.split(' ')
+
+  let sum = 0;
+
+  for(let i = 0; i < dd18.length; i++){
+    sum += parseInt(dd18[i], 10)
+    console.log(parseInt(dd18[i], 10))
+  }
+  console.log(sum/dd18.length)
+
